d90e2569d82d59085880d4f4012622b5f337ed53
Kevin Malakoff <kmalakoff@gmail.com>
2024-11-29 08:41:01 -0800

    Upgrade modules

--- test/spec/index.test.js
+++ test/spec/index.test.js
@@ -1,84 +1,84 @@
-var assert = require('assert');
+const assert = require('assert');
 
-var pathKey = require('../..');
+const pathKey = require('env-path-key');
 
-describe('darwin', function () {
-  it('no env', function () {
-    var key = pathKey({ platform: 'darwin' });
+describe('darwin', () => {
+  it('no env', () => {
+    const key = pathKey({ platform: 'darwin' });
     assert.equal(key, 'PATH');
   });
 
-  it('empty env', function () {
-    var key = pathKey({ platform: 'darwin', env: {} });
+  it('empty env', () => {
+    const key = pathKey({ platform: 'darwin', env: {} });
     assert.equal(key, 'PATH');
   });
 
-  it('ignores env', function () {
-    var key = pathKey({ platform: 'darwin', env: { Path: 'Not Used' } });
+  it('ignores env', () => {
+    const key = pathKey({ platform: 'darwin', env: { Path: 'Not Used' } });
     assert.equal(key, 'PATH');
   });
 
-  it('ignores env no path', function () {
-    var key = pathKey({ platform: 'darwin', env: {} });
+  it('ignores env no path', () => {
+    const key = pathKey({ platform: 'darwin', env: {} });
     assert.equal(key, 'PATH');
   });
 });
 
-describe('win32', function () {
-  it('empty env', function () {
-    var key = pathKey({ platform: 'win32', env: {} });
+describe('win32', () => {
+  it('empty env', () => {
+    const key = pathKey({ platform: 'win32', env: {} });
     assert.equal(key, 'Path');
   });
 
-  it('finds path', function () {
-    var key = pathKey({ platform: 'win32', env: { path: 'Used' } });
+  it('finds path', () => {
+    const key = pathKey({ platform: 'win32', env: { path: 'Used' } });
     assert.equal(key, 'path');
   });
 
-  it('finds Path', function () {
-    var key = pathKey({ platform: 'win32', env: { Path: 'Used' } });
+  it('finds Path', () => {
+    const key = pathKey({ platform: 'win32', env: { Path: 'Used' } });
     assert.equal(key, 'Path');
   });
 
-  it('finds pAth', function () {
-    var key = pathKey({ platform: 'win32', env: { pAth: 'Used' } });
+  it('finds pAth', () => {
+    const key = pathKey({ platform: 'win32', env: { pAth: 'Used' } });
     assert.equal(key, 'pAth');
   });
 
-  it('finds paTh', function () {
-    var key = pathKey({ platform: 'win32', env: { paTh: 'Used' } });
+  it('finds paTh', () => {
+    const key = pathKey({ platform: 'win32', env: { paTh: 'Used' } });
     assert.equal(key, 'paTh');
   });
 
-  it('finds patH', function () {
-    var key = pathKey({ platform: 'win32', env: { patH: 'Used' } });
+  it('finds patH', () => {
+    const key = pathKey({ platform: 'win32', env: { patH: 'Used' } });
     assert.equal(key, 'patH');
   });
 
-  it('skips PATH if multiple exist', function () {
-    var key = pathKey({ platform: 'win32', env: { PATH: 'Not Used', path: 'Used' } });
+  it('skips PATH if multiple exist', () => {
+    const key = pathKey({ platform: 'win32', env: { PATH: 'Not Used', path: 'Used' } });
     assert.equal(key, 'path');
   });
 
-  it('selects PATH if only exist', function () {
-    var key = pathKey({ platform: 'win32', env: { PATH: 'Not Used' } });
+  it('selects PATH if only exist', () => {
+    const key = pathKey({ platform: 'win32', env: { PATH: 'Not Used' } });
     assert.equal(key, 'PATH');
   });
 });
 
-describe('native', function () {
-  it('no options', function () {
-    var key = pathKey();
+describe('native', () => {
+  it('no options', () => {
+    const key = pathKey();
     assert.equal(key.toUpperCase(), 'PATH');
   });
 
-  it('empty options', function () {
-    var key = pathKey({});
+  it('empty options', () => {
+    const key = pathKey({});
     assert.equal(key.toUpperCase(), 'PATH');
   });
 
-  it('empty env', function () {
-    var key = pathKey({ env: {} });
+  it('empty env', () => {
+    const key = pathKey({ env: {} });
     assert.equal(key, process.platform === 'win32' ? 'Path' : 'PATH');
   });
 });
