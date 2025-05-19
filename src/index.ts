898ad6dccf01dde590dd08ac968df5c5a8dc8e46
Kevin Malakoff <kmalakoff@gmail.com>
2025-05-19 13:14:37 -0700

    Upgrade to mandatory typescript extensions

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import startsCaseInsensitiveFn from './startsCaseInsensitiveFn.mjs';
+import startsCaseInsensitiveFn from './startsCaseInsensitiveFn.js';
 
 const startsPath = startsCaseInsensitiveFn('path');
 
@@ -12,7 +12,12 @@ function windowsPathKey(env) {
   return pathKey;
 }
 
-export default (options) => {
+export interface Options {
+  platform?: string;
+  env?: NodeJS.ProcessEnv;
+}
+
+export default (options: Options): string => {
   const platform = options ? options.platform || process.platform : process.platform;
   if (platform !== 'win32') return 'PATH';
   const env = options ? options.env || process.env : process.env;
